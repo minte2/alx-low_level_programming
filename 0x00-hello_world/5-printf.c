98be9844fb5196d2472ae1c3a983afdb882ad60c
minte2 <mintejohn75@gmail.com>
2022-07-04 13:52:23 -0700

    push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,4 +5,7 @@
  * Return: 0 after printing the function
  */
 int main(void)
-	printf("with proper grammar, but the outcome is a pice of art,
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+},

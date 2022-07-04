1baf5afb2b5fc73fdf897c68d7149cc2695033f2
minte2 <mintejohn75@gmail.com>
2022-07-04 13:36:28 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  *
  * Return: 0 after printing the function
  */
-int main(void){
-	puts("\" programming is like building a multilingual puzzle");
-	 return(0);
-	 }
+int main(void)
+{
+puts("programming is like building a multilingual puzzle\n");
+return (0);
+}

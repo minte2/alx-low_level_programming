48bc9d0080fbe7991e0946ea0e17cbc21542d40b
minte2 <mintejohn75@gmail.com>
2022-07-05 09:17:40 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints the string  "_putchar" followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c[] = "_putchar";
+int i;
+for (i = 0; i <= 7; i++)
+	_putchar(c[i]);
+_putchar(c[i]);
+return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0x02 c function ana nested loop

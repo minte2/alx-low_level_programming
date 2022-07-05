432ec688d4a7e0ab1064000f2bee79c654e3343b
minte2 <mintejohn75@gmail.com>
2022-07-05 13:24:12 -0700

    alphx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase.
+ * Return : void .
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+while (i <= 9)
+{
+char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	i++;
+}
+}

b823c8e1b7f382e7aead30a8eb45ecdee8a5b114
minte2 <mintejohn75@gmail.com>
2022-07-14 01:48:21 -0700

    new

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,33 @@
 #include "main.h"
 
 /**
- *_strncat - concatinate 2 strings.
+ *_strncpy - copy src into dest.
  *@dest: first string.
  *@src: second string.
  *@n: the number of bytes to use from src.
  *Return: string.
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, k = 0;
 
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[k] != '\0' && n > k)
+	while (n > k)
 	{
-		dest[i] = src[k];
-		k++;
-		i++;
-	}
-	if (n > 0)
-	{
-		dest[i] = '\0';
+		if (src[k] == '\0')
+		{
+			for (; k < n; k++)
+			{
+				dest[i] = '\0';
+				i++;
+			}
+		}
+		else
+		{
+			dest[i] = src[k];
+			k++;
+			i++;
+		}
 	}
 
 	return (dest);
